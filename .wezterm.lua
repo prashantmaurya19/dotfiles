9abd4cc4511a9ff30b20e91531c8f68f68aa5907
Prashant Maurya <prashantmaurya1009@gmail.com>
2025-04-06 17:29:20 +0530

    littel tab_bar change in wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,12 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
@@ -45,7 +51,7 @@ config.colors = {
     -- The active tab is the one that has focus in the window
     active_tab = {
       -- The color of the background area for the tab
-      bg_color = "#2b2042",
+      bg_color = "#ff2042",
       -- The color of the text for the tab
       fg_color = "#c0c0c0",
 
@@ -177,34 +183,61 @@ config.key_tables = {
 config.use_fancy_tab_bar = false
 config.status_update_interval = 1000
 config.tab_bar_at_bottom = true
--- wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
---   local title = tab.tab_title
+local function tab_title(tab_info)
+  local title = tab_info.tab_title
+  -- if the tab title is explicitly set, take that
+  if title and #title > 0 then
+    return title
+  end
+  -- Otherwise, use the title from the active pane
+  -- in that tab
+  return tab_info.active_pane.title
+end
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local edge_background = "#0b0022"
+  local background = "#1b1032"
+  local foreground = "#808080"
 
---   if not title or title == "" then
---     -- local pane = tab.active_pane
---     title = tab.active_pane:get_foreground_process_name()
---     -- if pane then
---     --   local cmd = pane:get_foreground_process_name()
---     --   if cmd then
---     --     title = string.gsub(cmd, "(.*[/\\])(.*)", "%2")
---     --   else
---     --     title = "Shell" -- Default tab title if no command is running
---     --   end
---     -- else
---     --   title = "New Tab" -- Default tab title if pane is not available
---     -- end
---   end
---   local title_len = 5
---   if #title > title_len then
---     title = string.sub(title, 1, title_len) .. "~"
---   end
---   -- title = "New Tab" -- Default tab title if pane is not available
---   return {
---     { Text = " " .. (tab.tab_index + 1) .. "-" .. title .. " " .. #title },
---     -- { Text = "title " },
---   }
--- end)
+  if tab.is_active then
+    background = "#2b2042"
+    foreground = "#c0c0c0"
+  elseif hover then
+    background = "#3b3052"
+    foreground = "#909090"
+  end
+
+  local edge_foreground = background
+
+  local title = tab_title(tab)
+
+  -- ensure that the titles fit in the available space,
+  -- and that we have room for the edges.
+  title = wezterm.truncate_right(title, max_width - 2)
+
+  return {
+    { Background = { Color = edge_foreground } },
+    { Foreground = { Color = edge_background } },
+    { Text = SOLID_LEFT_ARROW },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = (tab.tab_id + 1) .. ":" .. title },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_RIGHT_ARROW },
+  }
+end)
 wezterm.on("update-status", function(window, pane)
+  -- Workspace name
+  local stat = window:active_workspace()
+  local stat_color = "#f7768e"
+  if window:active_key_table() then
+    stat = window:active_key_table()
+    stat_color = "#7dcfff"
+  end
+  if window:leader_is_active() then
+    stat = "LDR"
+    stat_color = "#bb9af7"
+  end
   local cmd = pane:get_foreground_process_name()
   cmd = cmd and string.gsub(cmd, "(.*[/\\])(.*)", "%2") or ""
 
@@ -224,20 +257,6 @@ wezterm.on("update-status", function(window, pane)
     { Text = wezterm.nerdfonts.md_clock .. " " .. time },
     { Text = "  " },
   }))
-end)
-
-wezterm.on("update-status", function(window, pane)
-  -- Workspace name
-  local stat = window:active_workspace()
-  local stat_color = "#f7768e"
-  if window:active_key_table() then
-    stat = window:active_key_table()
-    stat_color = "#7dcfff"
-  end
-  if window:leader_is_active() then
-    stat = "LDR"
-    stat_color = "#bb9af7"
-  end
 
   -- Left status (left of the tab line)
   window:set_left_status(wezterm.format({
